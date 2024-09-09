c5bd718a2aef7f34f2acefa997c53d7f192b97ef
Nerwin Hernando <nerwinc.hernando@gmail.com>
2024-09-10 05:32:35 +0800

    add generated pages

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,31 @@
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export const links = () => [
+  { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" },
+];
+
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
